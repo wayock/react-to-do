b075199301cc8a7ba4ef8482010231254e360992
Matthew Wayock <wayock@gmail.com>
2019-02-26 17:01:13 -0500

    react-to-do state and props checkpoint completed.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {description: 'React-To-Do Component Assignment', isCompleted: true },
+        {description: 'Workout', isCompleted: false },
+        {description: 'React-To-Do Props & State Checkpoint', isCompleted: false}
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <ul>
-          <ToDo />
-          <ToDo />
+          { this.state.todos.map( (todo, index) =>
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+          )}
         </ul>
       </div>
     );
